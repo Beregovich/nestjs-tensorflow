c3bfd19343e809c8bf297caa7b3db79dd9075f78
Mikhail Beregovich <beregovich@gmail.com>
2023-10-05 17:16:54 +0300

    feat: lessons module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { DatabaseModule } from './modules/database/database.module';
 import { SettingsModule } from './settings/settings.module';
 import { FaqModule } from './modules/faq/faq.module';
 import { UserModule } from './modules/users/user.module';
+import { LessonsModule } from './modules/lessons/lessons.module';
 
 @Module({
-  imports: [DatabaseModule, SettingsModule, FaqModule, UserModule],
+  imports: [
+    DatabaseModule,
+    SettingsModule,
+    FaqModule,
+    UserModule,
+    LessonsModule,
+  ],
   controllers: [],
   providers: [],
 })
