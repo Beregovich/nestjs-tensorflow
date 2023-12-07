7c66db7a9fbe811bef099ba8960ce7852b26e261
Mikhail Beregovich <92076077+Beregovich@users.noreply.github.com>
2023-12-07 21:42:22 +0300

    Develop (#5)
    chore(refactoring) config module refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './modules/database/database.module';
-import { SettingsModule } from './settings/settings.module';
 import { FaqModule } from './modules/faq/faq.module';
 import { UserModule } from './modules/users/user.module';
+import { ConfigModule } from './settings/config.module';
 
 @Module({
-  imports: [DatabaseModule, SettingsModule, FaqModule, UserModule],
+  imports: [DatabaseModule, ConfigModule, FaqModule, UserModule],
   controllers: [],
   providers: [],
 })
